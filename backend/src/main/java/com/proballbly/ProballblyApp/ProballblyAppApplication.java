0cddbcaddda166ff4bebf55b304761354dda3c7f
Kamil Maksymowicz <maxio090124@interia.pl>
2021-03-23 14:04:04 +0100

    Create Spring app

---
+++ backend/src/main/java/com/proballbly/ProballblyApp/ProballblyAppApplication.java
@@ -0,0 +1,13 @@
+package com.proballbly.ProballblyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProballblyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProballblyAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proballbly/ProballblyApp/ProballblyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proballbly.ProballblyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProballblyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
