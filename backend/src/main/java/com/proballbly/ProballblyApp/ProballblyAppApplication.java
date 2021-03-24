65298a36c6a118b8b52dfcd9ffca80b834c62d0b
Kamil Maksymowicz <maxio090124@interia.pl>
2021-03-24 13:13:56 +0100

    localhost, security and entity config

--- backend/src/main/java/com/proballbly/ProballblyApp/ProballblyAppApplication.java
+++ backend/src/main/java/com/proballbly/ProballblyApp/ProballblyAppApplication.java
@@ -1,13 +1,20 @@
 package com.proballbly.ProballblyApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@ComponentScan
 public class ProballblyAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProballblyAppApplication.class, args);
+
 	}
 
 }
